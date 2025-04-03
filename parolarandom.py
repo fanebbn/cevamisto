a8988728d981548ea7b8121f445912ce6f20406a
stefan <s.lang@student.utwente.nl>
2025-04-03 12:49:31 +0200

    parola random

---
+++ parolarandom.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(length))
+    return password
+
+print("🔐 Your random password is:")
+print(generate_password())
