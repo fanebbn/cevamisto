d1af66a1c766e58491695c155dcef293ad38e792
stefan <s.lang@student.utwente.nl>
2025-04-03 11:49:48 +0200

    add motivational

---
+++ motivation.py
@@ -0,0 +1,13 @@
+import random
+
+quotes = [
+    "Believe in yourself and all that you are.",
+    "The secret to getting ahead is getting started.",
+    "Push yourself, because no one else is going to do it for you.",
+    "Great things never come from comfort zones.",
+    "Dream it. Wish it. Do it.",
+    "Don't watch the clock; do what it does. Keep going."
+]
+
+print("💬 Motivational Quote of the Day:\n")
+print(random.choice(quotes))
